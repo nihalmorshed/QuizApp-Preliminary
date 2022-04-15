ce3f7423428b45801dc1caa8cbd11bd4819195cd
Nihal Morshed <nilumorshed@gmail.com>
2022-04-16 03:43:05 +0600

    16 April 2022

--- lib/answersfile.dart
+++ lib/answersfile.dart
@@ -12,9 +12,13 @@ class Answers extends StatelessWidget {
       width: double.infinity,
       child: RaisedButton(
         onPressed: selecthandler,
+        elevation: 5,
         color: Colors.blueGrey,
         textColor: Colors.white,
+        splashColor: Colors.red,
         child: Text(anstxt),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(16.0))),
       ),
     );
   }
