1eccd5c2e74321c82bdbe79eb97ca5d9add84334
Nihal Morshed <nilumorshed@gmail.com>
2022-04-22 17:56:14 +0600

    22 april 2022 1755

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: import_of_legacy_library_into_null_safe
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:splashscreen/splashscreen.dart';
 import './login.dart';
 
 void main() async {
@@ -9,17 +12,52 @@ void main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _Myappstate();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Splash Screen',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: Splash2(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
 
-class _Myappstate extends State<MyApp> {
+class Splash2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 6,
+      navigateAfterSeconds: new SecondScreen(),
+      title: new Text(
+        'Quiz\nApp',
+        textScaleFactor: 8,
+        style: TextStyle(color: Colors.orange),
+      ),
+      image: new Image.network(
+          "https://i.pinimg.com/736x/b7/96/8e/b7968e44ab4e1cf2f987cb8db94b7353.jpg"),
+      backgroundColor: Colors.black,
+      loadingText: Text("Loading"),
+      photoSize: 100.0,
+      loaderColor: Colors.blue,
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return loginregister();
   }
 }
+
+// class _Myappstate extends State<MyApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return loginregister();
+//     //return splash();
+//   }
+// }
