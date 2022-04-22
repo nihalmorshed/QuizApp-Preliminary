ee20d3ca52de9daa7799cc8cad5bbe98b3721ee1
Nihal Morshed <nilumorshed@gmail.com>
2022-04-22 17:56:55 +0600

    Revert "22 april 2022 1755"
    This reverts commit 1eccd5c2e74321c82bdbe79eb97ca5d9add84334.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-// ignore_for_file: import_of_legacy_library_into_null_safe
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:splashscreen/splashscreen.dart';
 import './login.dart';
 
 void main() async {
@@ -12,52 +9,17 @@ void main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Splash Screen',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: Splash2(),
-      debugShowCheckedModeBanner: false,
-    );
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _Myappstate();
   }
 }
 
-class Splash2 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return SplashScreen(
-      seconds: 6,
-      navigateAfterSeconds: new SecondScreen(),
-      title: new Text(
-        'Quiz\nApp',
-        textScaleFactor: 8,
-        style: TextStyle(color: Colors.orange),
-      ),
-      image: new Image.network(
-          "https://i.pinimg.com/736x/b7/96/8e/b7968e44ab4e1cf2f987cb8db94b7353.jpg"),
-      backgroundColor: Colors.black,
-      loadingText: Text("Loading"),
-      photoSize: 100.0,
-      loaderColor: Colors.blue,
-    );
-  }
-}
-
-class SecondScreen extends StatelessWidget {
+class _Myappstate extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return loginregister();
   }
 }
-
-// class _Myappstate extends State<MyApp> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return loginregister();
-//     //return splash();
-//   }
-// }
